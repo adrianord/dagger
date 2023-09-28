906b062990731313c7b120b53cf18de32f978905
Adrian Ordonez <adrianord@gmail.com>
2023-09-28 16:50:16 -0400

    chore: add dagger-replay experimental tool

---
+++ cmd/dagger-replay/README.md
@@ -0,0 +1,10 @@
+# dagger-replay
+
+**Experimental** tool to replay the journal file from `_EXPERIMENTAL_DAGGER_JOURNAL` in the inline and internal TUI.
+
+## Usage
+```console
+dagger-replay example.journal # For inline TUI
+_EXPERIMENTAL_DAGGER_INTERACTIVE_TUI=1 ./dagger-replay example.journal # For interactive TUI
+```
+
---
+++ cmd/dagger-replay/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/dagger/dagger/internal/tui"
+	"github.com/spf13/cobra"
+	"github.com/vito/progrock"
+)
+
+func main() {
+	replayCmd := &cobra.Command{
+		Use:          "replay [journal]",
+		Long:         "Replay a journal file",
+		Short:        "Replay a journal file",
+		Args:         cobra.MinimumNArgs(1),
+		SilenceUsage: true,
+		RunE:         Replay,
+	}
+
+	if err := replayCmd.Execute(); err != nil {
+		panic(err)
+	}
+}
+
+func Replay(cmd *cobra.Command, args []string) error {
+	if os.Getenv("_EXPERIMENTAL_DAGGER_INTERACTIVE_TUI") != "" {
+		return replayInteractiveTUI(cmd.Context(), args[0])
+	} else {
+		return replayInternalTUI(args[0])
+	}
+}
+
+func replayInternalTUI(journal string) error {
+	tape := progrock.NewTape()
+	tape.ShowAllOutput(true)
+	defer tape.Close()
+
+	iterateOverEvents(journal, func(event *progrock.StatusUpdate) error {
+		return tape.WriteStatus(event)
+	})
+	err := tape.Render(os.Stdout, progrock.DefaultUI())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func replayInteractiveTUI(ctx context.Context, journal string) error {
+	progR, progW := progrock.Pipe()
+
+	ctx, quit := context.WithCancel(ctx)
+	defer quit()
+
+	program := tea.NewProgram(tui.New(quit, progR), tea.WithAltScreen())
+
+	tuiDone := make(chan error, 1)
+	go func() {
+		_, err := program.Run()
+		tuiDone <- err
+	}()
+
+	iterateOverEvents(journal, func(event *progrock.StatusUpdate) error {
+		return progW.WriteStatus(event)
+	})
+	progW.Close()
+
+	tuiErr := <-tuiDone
+	return tuiErr
+}
+
+func iterateOverEvents(journal string, cb func(event *progrock.StatusUpdate) error) error {
+	f, err := os.Open(journal)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	dec := json.NewDecoder(f)
+	for {
+		var update progrock.StatusUpdate
+		if err := dec.Decode(&update); err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+
+			return err
+		}
+
+		err := cb(&update)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
